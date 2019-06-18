5f53275a8424dc682cc03cd610380737d0e94a10
Javier Rizzo <javierrizzoa@gmail.com>
2019-06-18 13:17:10 -0700

    Add license to client source

--- client/src/ContadorLikes.ino
+++ client/src/ContadorLikes.ino
@@ -1,3 +1,20 @@
+/*
+   This file is part of contador-de-likes.
+
+   contador-de-likes is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   contador-de-likes is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with contador-de-likes.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Adafruit_LEDBackpack_RK.h>
 #include "Particle.h"
 #include "Adafruit_GFX_RK.h"
